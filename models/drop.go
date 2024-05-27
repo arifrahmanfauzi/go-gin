551387e39b89a031ad1a5c54f04e20f264e83297
arif rahman fauzi <arif.rahman1780@gmail.com>
2024-05-27 09:20:18 +0700

    adding zerolog

--- models/drop.go
+++ models/drop.go
@@ -5,4 +5,5 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 type Drops struct {
 	ID             primitive.ObjectID `bson:"_id" json:"id"`
 	DispatchNumber string             `bson:"dispatchNumber" json:"dispatch_number"`
+	Job            string
 }
